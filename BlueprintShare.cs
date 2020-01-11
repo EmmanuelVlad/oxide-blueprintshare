fb36afdf7d89563348a3c0e866b6f8ffa74040be
Emmanuel Vlad <emmanuelvlad@gmail.com>
2020-01-11 19:39:31 +0100

    ✨ new option, less data, new message on learn

--- BlueprintShare.cs
+++ BlueprintShare.cs
@@ -10,345 +10,328 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-	[Info("Blueprint Share", "c_creep", "1.1.3")]
-	[Description("Allows players to share researched blueprints with their friends, clan or team")]
+    [Info("Blueprint Share", "c_creep", "1.1.3")]
+    [Description("Allows players to share researched blueprints with their friends, clan or team")]
 
-	class BlueprintShare : CovalencePlugin
-	{
-		#region Fields
+    class BlueprintShare : CovalencePlugin
+    {
+        #region Fields
 
-		[PluginReference] private Plugin Clans, ClansReborn, Friends;
+        [PluginReference] private Plugin Clans, ClansReborn, Friends;
 
-		private Dictionary<string, bool> sharingEnabledList = new Dictionary<string, bool>();
+		private List<string> playersDataList = new List<string>();
 
-		private DynamicConfigFile playerData;
+        private DynamicConfigFile playersData;
 
-		private bool sharingEnabled = true, clansEnabled = true, friendsEnabled = true, teamsEnabled = true;
+        private bool sharingEnabled = true, clansEnabled = true, friendsEnabled = true, teamsEnabled = true, enabledByDefault = true;
 
-		#endregion
+        #endregion
 
-		#region Oxide Hooks
+        #region Oxide Hooks
 
-		private void Init()
-		{
+        private void Init()
+        {
 			permission.RegisterPermission("blueprintshare.toggle", this);
 
-			LoadDefaultConfig();
+            LoadDefaultConfig();
 
-			playerData = Interface.Oxide.DataFileSystem.GetFile("BlueprintShare");
+            playersData = Interface.Oxide.DataFileSystem.GetFile("BlueprintShare");
 
-			sharingEnabledList = playerData.ReadObject<Dictionary<string, bool>>();
-		}
+            playersDataList = playersData.ReadObject<List<string>>();
+        }
 
-		private void OnPlayerInit(BasePlayer player)
-		{
-			if (sharingEnabledList.ContainsKey(player.UserIDString))
-			{
-				sharingEnabled = sharingEnabledList[player.UserIDString];
-			}
-			else
-			{
-				sharingEnabledList.Add(player.UserIDString, sharingEnabled);
+        private void OnPlayerInit(BasePlayer player)
+        {
+			sharingEnabled = playersDataList.Contains(player.UserIDString) ? !enabledByDefault : enabledByDefault;
+        }
 
-				playerData.WriteObject(sharingEnabledList);
-			}
-		}
+        private void OnItemAction(Item item, string action, BasePlayer player)
+        {
+            if (player != null && action == "study" && (InClan(player.userID) || HasFriends(player.userID) || InTeam(player.userID)) && item.IsBlueprint() && sharingEnabled)
+            {
+                var itemShortName = item.blueprintTargetDef.shortname;
 
-		private void OnItemAction(Item item, string action, BasePlayer player)
-		{
-			if (player != null && action == "study" && (InClan(player.userID) || HasFriends(player.userID) || InTeam(player.userID)) && item.IsBlueprint() && sharingEnabled)
-			{
-				var itemShortName = item.blueprintTargetDef.shortname;
+                if (string.IsNullOrEmpty(itemShortName)) return;
 
-				if (string.IsNullOrEmpty(itemShortName)) return;
+                item.Remove();
 
-				item.Remove();
+                UnlockBlueprint(player, itemShortName);
+            }
+        }
 
-				UnlockBlueprint(player, itemShortName);
-			}
-		}
+        #endregion
 
-		#endregion
+        #region Config
 
-		#region Config
+        protected override void LoadDefaultConfig()
+        {
+            Config["ClansEnabled"] = clansEnabled = GetConfigValue("ClansEnabled", true);
+            Config["FriendsEnabled"] = friendsEnabled = GetConfigValue("FriendsEnabled", true);
+            Config["TeamsEnabled"] = teamsEnabled = GetConfigValue("TeamsEnabled", true);
+            Config["EnableByDefault"] = enabledByDefault = GetConfigValue("EnableByDefault", true);
 
-		protected override void LoadDefaultConfig()
-		{
-			Config["ClansEnabled"] = clansEnabled = GetConfigValue("ClansEnabled", true);
-			Config["FriendsEnabled"] = friendsEnabled = GetConfigValue("FriendsEnabled", true);
-			Config["TeamsEnabled"] = teamsEnabled = GetConfigValue("TeamsEnabled", true);
+            SaveConfig();
+        }
 
-			SaveConfig();
-		}
+        private T GetConfigValue<T>(string name, T defaultValue)
+        {
+            return Config[name] == null ? defaultValue : (T)Convert.ChangeType(Config[name], typeof(T));
+        }
 
-		private T GetConfigValue<T>(string name, T defaultValue)
-		{
-			return Config[name] == null ? defaultValue : (T)Convert.ChangeType(Config[name], typeof(T));
-		}
+        #endregion
 
-		#endregion
+        #region Localization
 
-		#region Localization
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["Prefix"] = "[#D85540][Blueprint Share] [/#]",
+                ["ArgumentsErrorMessage"] = "Error, incorrect arguments. Try /bs help.",
+                ["HelpMessage"] = "[#D85540]Blueprint Share Help:[/#]\n\n[#D85540]/bs toggle[/#] - Toggles the sharing of blueprints.",
+                ["ToggleOnMessage"] = "You have enabled sharing blueprints.",
+                ["ToggleOffMessage"] = "You have disabled sharing blueprints.",
+                ["NoPermissionMessage"] = "You don't have permission to use this command!",
+				["NewBlueprintLearned"] = "[#55aaff]{0} learned a new blueprint: <size=18>{1}</size>[/#]"
+            }, this);
+        }
 
-		protected override void LoadDefaultMessages()
-		{
-			lang.RegisterMessages(new Dictionary<string, string>
-			{
-				["Prefix"] = "[#D85540][Blueprint Share] [/#]",
-				["ArgumentsErrorMessage"] = "Error, incorrect arguments. Try /bs help.",
-				["HelpMessage"] = "[#D85540]Blueprint Share Help:[/#]\n\n[#D85540]/bs toggle[/#] - Toggles the sharing of blueprints.",
-				["ToggleOnMessage"] = "You have enabled sharing blueprints.",
-				["ToggleOffMessage"] = "You have disabled sharing blueprints.",
-				["NoPermissionMessage"] = "You don't have permission to use this command!"
-			}, this);
-		}
+        private string GetLangValue(string key, string id = null, params object[] args) => covalence.FormatText(string.Format(lang.GetMessage(key, this, id), args));
 
-		private string GetLangValue(string key, string id = null)
-		{
-			return covalence.FormatText(lang.GetMessage(key, this, id));
-		}
+        #endregion
 
-		#endregion
+        #region General Methods
 
-		#region General Methods
+        private void UnlockBlueprint(BasePlayer player, string itemShortName)
+        {
+            if (player == null) return;
+            if (string.IsNullOrEmpty(itemShortName)) return;
 
-		private void UnlockBlueprint(BasePlayer player, string itemShortName)
-		{
-			if (player == null) return;
-			if (string.IsNullOrEmpty(itemShortName)) return;
+            var playerUID = player.userID;
 
-			var playerUID = player.userID;
+            var playersToShareWith = new List<BasePlayer>();
 
-			var playersToShareWith = new List<BasePlayer>();
+            if (clansEnabled && (Clans != null || ClansReborn != null) && InClan(playerUID))
+            {
+                playersToShareWith.AddRange(GetClanMembers(playerUID));
+            }
 
-			if (clansEnabled && (Clans != null || ClansReborn != null) && InClan(playerUID))
-			{
-				playersToShareWith.AddRange(GetClanMembers(playerUID));
-			}
+            if (friendsEnabled && Friends != null && HasFriends(playerUID))
+            {
+                playersToShareWith.AddRange(GetFriends(playerUID));
+            }
+            
+            if (teamsEnabled && InTeam(playerUID))
+            {
+                playersToShareWith.AddRange(GetTeamMembers(playerUID));
+            }
 
-			if (friendsEnabled && Friends != null && HasFriends(playerUID))
-			{
-				playersToShareWith.AddRange(GetFriends(playerUID));
-			}
+            foreach (BasePlayer sharePlayer in playersToShareWith)
+            {
+                if (sharePlayer != null)
+                {
+                    var blueprintComponent = sharePlayer.blueprints;
 
-			if (teamsEnabled && InTeam(playerUID))
-			{
-				playersToShareWith.AddRange(GetTeamMembers(playerUID));
-			}
+                    if (blueprintComponent == null) return;
 
-			foreach (BasePlayer sharePlayer in playersToShareWith)
-			{
-				if (sharePlayer != null)
-				{
-					var blueprintComponent = sharePlayer.blueprints;
+                    var itemDefinition = GetItemDefinition(itemShortName);
 
-					if (blueprintComponent == null) return;
+                    if (itemDefinition == null) return;
 
-					var itemDefinition = GetItemDefinition(itemShortName);
+                    blueprintComponent.Unlock(itemDefinition);
 
-					if (itemDefinition == null) return;
-
-					blueprintComponent.Unlock(itemDefinition);
-
-					var soundEffect = new Effect("assets/prefabs/deployable/research table/effects/research-success.prefab", sharePlayer.transform.position, Vector3.zero);
-
-					if (soundEffect == null) return;
-
-					EffectNetwork.Send(soundEffect, sharePlayer.net.connection);
-				}
-			}
-		}
-
-		private ItemDefinition GetItemDefinition(string itemShortName)
-		{
-			if (string.IsNullOrEmpty(itemShortName)) return null;
-
-			var itemDefinition = ItemManager.FindItemDefinition(itemShortName.ToLower());
-
-			return itemDefinition;
-		}
-
-		#endregion
-
-		#region Clan Methods
-
-		private bool InClan(ulong playerUID)
-		{
-			if (ClansReborn == null && Clans == null) return false;
-
-			var clanName = Clans?.Call<string>("GetClanOf", playerUID);
-
-			return clanName != null;
-		}
-
-		private List<BasePlayer> GetClanMembers(ulong playerUID)
-		{
-			var membersList = new List<BasePlayer>();
-
-			var clanName = Clans?.Call<string>("GetClanOf", playerUID);
-
-			if (!string.IsNullOrEmpty(clanName))
-			{
-				var clan = Clans?.Call<JObject>("GetClan", clanName);
-
-				if (clan != null && clan is JObject)
-				{
-					var members = (clan as JObject).GetValue("members");
-
-					if (members != null && members is JArray)
+					if (player.userID != sharePlayer.userID)
 					{
-						foreach (var member in (JArray)members)
-						{
-							ulong clanMemberUID;
-
-							if (!ulong.TryParse(member.ToString(), out clanMemberUID)) continue;
-
-							var clanMember = RustCore.FindPlayerById(clanMemberUID);
-
-							membersList.Add(clanMember);
-						}
+						sharePlayer.Command("chat.add", 0, 0, GetLangValue("NewBlueprintLearned", sharePlayer.UserIDString, player.displayName, itemDefinition.displayName.translated));
 					}
-				}
-			}
-			return membersList;
-		}
 
-		#endregion
+                    var soundEffect = new Effect("assets/prefabs/deployable/research table/effects/research-success.prefab", sharePlayer.transform.position, Vector3.zero);
 
-		#region Friends Methods
+                    if (soundEffect == null) return;
 
-		private bool HasFriends(ulong playerUID)
-		{
-			if (Friends == null) return false;
+                    EffectNetwork.Send(soundEffect, sharePlayer.net.connection);
+                }
+            }
+        }
 
-			var friendsList = Friends.Call<ulong[]>("GetFriends", playerUID);
+        private ItemDefinition GetItemDefinition(string itemShortName)
+        {
+            if (string.IsNullOrEmpty(itemShortName)) return null;
 
-			return friendsList != null && friendsList.Length != 0;
-		}
+            var itemDefinition = ItemManager.FindItemDefinition(itemShortName.ToLower());
 
-		private List<BasePlayer> GetFriends(ulong playerUID)
-		{
-			var friendsList = new List<BasePlayer>();
+            return itemDefinition;
+        }
 
-			var friends = Friends.Call<ulong[]>("GetFriends", playerUID);
+        #endregion
 
-			foreach (var friendUID in friends)
-			{
-				var friend = RustCore.FindPlayerById(friendUID);
+        #region Clan Methods
 
-				friendsList.Add(friend);
-			}
+        private bool InClan(ulong playerUID)
+        {
+            if (ClansReborn == null && Clans == null) return false;
 
-			return friendsList;
-		}
+            var clanName = Clans?.Call<string>("GetClanOf", playerUID);
 
-		#endregion
+            return clanName != null;
+        }
 
-		#region Team Methods
+        private List<BasePlayer> GetClanMembers(ulong playerUID)
+        {
+            var membersList = new List<BasePlayer>();
 
-		private bool InTeam(ulong playerUID)
-		{
-			var player = RustCore.FindPlayerById(playerUID);
+            var clanName = Clans?.Call<string>("GetClanOf", playerUID);
 
-			var playersCurrentTeam = RelationshipManager.Instance.FindTeam(player.currentTeam);
+            if (!string.IsNullOrEmpty(clanName))
+            {
+                var clan = Clans?.Call<JObject>("GetClan", clanName);
 
-			return playersCurrentTeam != null;
-		}
+                if (clan != null && clan is JObject)
+                {
+                    var members = (clan as JObject).GetValue("members");
 
-		private List<BasePlayer> GetTeamMembers(ulong playerUID)
-		{
-			var membersList = new List<BasePlayer>();
+                    if (members != null && members is JArray)
+                    {
+                        foreach (var member in (JArray)members)
+                        {
+                            ulong clanMemberUID;
 
-			var player = RustCore.FindPlayerById(playerUID);
+                            if (!ulong.TryParse(member.ToString(), out clanMemberUID)) continue;
 
-			var playersCurrentTeam = RelationshipManager.Instance.FindTeam(player.currentTeam);
+                            var clanMember = RustCore.FindPlayerById(clanMemberUID);
 
-			var teamMembers = playersCurrentTeam.members;
+                            membersList.Add(clanMember);
+                        }
+                    }
+                }
+            }
+            return membersList;
+        }
 
-			foreach (var teamMemberUID in teamMembers)
-			{
-				var teamMember = RustCore.FindPlayerById(teamMemberUID);
+        #endregion
 
-				membersList.Add(teamMember);
-			}
+        #region Friends Methods
 
-			return membersList;
-		}
+        private bool HasFriends(ulong playerUID)
+        {
+            if (Friends == null) return false;
 
-		#endregion
+            var friendsList = Friends.Call<ulong[]>("GetFriends", playerUID);
 
-		#region Chat Commands
+            return friendsList != null && friendsList.Length != 0;
+        }
 
-		[Command("blueprintshare", "bs")]
-		private void ToggleCommand(IPlayer player, string command, string[] args)
-		{
-			var playerUID = player.Id;
+        private List<BasePlayer> GetFriends(ulong playerUID)
+        {
+            var friendsList = new List<BasePlayer>();
 
-			if (args.Length < 1)
-			{
-				player.Reply(GetLangValue("Prefix", playerUID) + GetLangValue("ArgumentsErrorMessage", playerUID));
+            var friends = Friends.Call<ulong[]>("GetFriends", playerUID);
 
-				return;
-			}
+            foreach (var friendUID in friends)
+            {
+                var friend = RustCore.FindPlayerById(friendUID);
 
-			switch (args[0].ToLower())
-			{
-				case "help":
-					{
-						player.Reply(GetLangValue("HelpMessage", playerUID));
+                friendsList.Add(friend);
+            }
 
-						break;
-					}
-				case "toggle":
-					{
-						if (permission.UserHasPermission(playerUID, "blueprintshare.toggle"))
-						{
-							if (sharingEnabled)
+            return friendsList;
+        }
+
+        #endregion
+
+        #region Team Methods
+
+        private bool InTeam(ulong playerUID)
+        {
+            var player = RustCore.FindPlayerById(playerUID);
+
+            var playersCurrentTeam = RelationshipManager.Instance.FindTeam(player.currentTeam);
+
+            return playersCurrentTeam != null;
+        }
+
+        private List<BasePlayer> GetTeamMembers(ulong playerUID)
+        {
+            var membersList = new List<BasePlayer>();
+
+            var player = RustCore.FindPlayerById(playerUID);
+
+            var playersCurrentTeam = RelationshipManager.Instance.FindTeam(player.currentTeam);
+
+            var teamMembers = playersCurrentTeam.members;
+
+            foreach (var teamMemberUID in teamMembers)
+            {
+                var teamMember = RustCore.FindPlayerById(teamMemberUID);
+
+                membersList.Add(teamMember);
+            }
+
+            return membersList;
+        }
+
+        #endregion
+
+        #region Chat Commands
+
+        [Command("blueprintshare", "bs")]
+        private void ToggleCommand(IPlayer player, string command, string[] args)
+        {
+            var playerUID = player.Id;
+
+            if (args.Length < 1)
+            {
+                player.Reply(GetLangValue("Prefix", playerUID) + GetLangValue("ArgumentsErrorMessage", playerUID));
+
+                return;
+            }
+
+            switch (args[0].ToLower())
+            {
+                case "help":
+                    {
+                        player.Reply(GetLangValue("HelpMessage", playerUID));
+
+                        break;
+                    }
+                case "toggle":
+                    {
+                        if (permission.UserHasPermission(playerUID, "blueprintshare.toggle"))
+                        {
+							player.Reply(GetLangValue("Prefix", playerUID) + GetLangValue((sharingEnabled) ? "ToggleOffMessage" : "ToggleOnMessage", playerUID));
+							sharingEnabled = !sharingEnabled;
+
+							if ((enabledByDefault && !sharingEnabled) ||
+								(!enabledByDefault && sharingEnabled))
 							{
-								sharingEnabled = false;
-
-								player.Reply(GetLangValue("Prefix", playerUID) + GetLangValue("ToggleOffMessage", playerUID));
+								playersDataList.Add(playerUID);
 							}
 							else
 							{
-								sharingEnabled = true;
-
-								player.Reply(GetLangValue("Prefix", playerUID) + GetLangValue("ToggleOnMessage", playerUID));
+								playersDataList.Remove(playerUID);
 							}
 
-							sharingEnabledList[playerUID] = sharingEnabled;
+                            playersData.WriteObject(playersDataList);
+                        }
+                        else
+                        {
+                            player.Reply(GetLangValue("Prefix", playerUID) + GetLangValue("NoPermissionMessage", playerUID));
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
 
-							playerData.WriteObject(sharingEnabledList);
-						}
-						else
-						{
-							player.Reply(GetLangValue("Prefix", playerUID) + GetLangValue("NoPermissionMessage", playerUID));
-						}
-						break;
-					}
-				default:
-					{
-						break;
-					}
-			}
-		}
+        #endregion
 
-		#endregion
+        #region API
 
-		#region API
+        private bool SharingEnabled(string playerUID) => playersDataList.Contains(playerUID) ? !enabledByDefault : enabledByDefault;
 
-		private bool SharingEnabled(string playerUID)
-		{
-			if (sharingEnabledList.ContainsKey(playerUID))
-			{
-				return sharingEnabledList[playerUID];
-			}
-			else
-			{
-				return false;
-			}
-		}
-
-		#endregion
-	}
+        #endregion
+    }
 }
